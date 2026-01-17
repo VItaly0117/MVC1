49e598532f6d44ab2c8f9e594fdd1dbc0a401fd2
Vitaly0117 <vitalik.kalinichenko123@gmail.com>
2026-01-18 01:45:58 +0200

    too

---
+++ MVC/Models/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC.Models;
+
+public class StoreContext : IdentityDbContext<User, IdentityRole<int>, int>
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<ImageUploaded> Images { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
+
+    public StoreContext(DbContextOptions<StoreContext> options)
+        : base(options)
+    {
+    }
+}
